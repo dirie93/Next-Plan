e8e53ba0281a2bc915e2af9e18b5ac7e8a8cbea1
dirie93 <mdirie19@gmail.com>
2023-05-13 22:38:49 +0100

    fixed issue with functionality, added in jQuery in HTML

--- script.js
+++ script.js
@@ -5,32 +5,33 @@ $(function () {
   // added an event listener that listens out for when button is 'clicked'
   $(".saveBtn").on("click", function () {
     var timeBlockId = $(this).parent().attr("id");
-    var userInput = $(this).siblings(".description").val(); // user input
-    localStorage.setItem(timeBlockId, userInput); // then the user input will be saved to local storage with the time block iD
+    var userInput = $(this).siblings(".description").val(); // User input
+    localStorage.setItem(timeBlockId, userInput); // Save user input to local storage with time block ID
   });
 
   var presentHour = new Date().getHours();
 
   $(".time-block").each(function () {
-    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]); // retrieve the hour from the time block ID
+    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]); // Retrieve the hour from the time block ID
 
     // this if-else code block determines what time block it is if a condition is met
     if (timeBlockHour < presentHour) {
       // condition: if  time block hour is < than the present hour, it is in the past
       $(this).addClass("past");
+      $(this).find(".description").addClass("past"); // Add class to textarea
     } else if (timeBlockHour === presentHour) {
       // condition: if  time block hour is = to  present hour, it is the present
-
       $(this).addClass("present");
+      $(this).find(".description").addClass("present"); // Add class to textarea
     } else {
       // condition: if  time block hour is > than the present hour, it is in the future
-
       $(this).addClass("future");
+      $(this).find(".description").addClass("future"); // Add class to textarea
     }
 
-    var storedInput = localStorage.getItem($(this).attr("id")); // Retrieves stored input from local storage based on the time block ID
+    var storedInput = localStorage.getItem($(this).attr("id")); // Retrieve stored input from local storage based on the time block ID
     if (storedInput) {
-      $(this).find(".description").val(storedInput);
+      $(this).find(".description").val(storedInput); // Set the stored input in the textarea
     }
   });
 });
